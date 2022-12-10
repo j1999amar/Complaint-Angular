1421f243c16807ff8451501e4246ffe8ad8695a2
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-10 13:42:57 +0530

    User Registation Data Api

--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-user-registration',
@@ -6,6 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-registration.component.css'],
 })
 export class UserRegistrationComponent {
+  constructor(private route:Router,private api:ApiService){}
   name = '';
   address = '';
   date=''
@@ -15,7 +19,7 @@ export class UserRegistrationComponent {
   password = '';
   confirmPassword = '';
   readValue = () => {
-    let data:any = [
+    let data:any = 
       {
         "name": this.name,
         "address": this.address,
@@ -25,8 +29,18 @@ export class UserRegistrationComponent {
         "userLogin": this.userLogin,
         "password": this.password,
         "confirmPassword": this.confirmPassword,
-      },
-    ];
-    console.log(data)
+      }
+    
+    if(this.userLogin!=""&&(this.password==this.confirmPassword)){
+      this.api.addUser(data).subscribe(
+        (response)=>{
+          console.log(response)
+        }
+      ) 
+      this.route.navigate(['/userLogin'])
+    }
+    else{
+      alert("Something Went Wrong...")
+    }
   };
 }
